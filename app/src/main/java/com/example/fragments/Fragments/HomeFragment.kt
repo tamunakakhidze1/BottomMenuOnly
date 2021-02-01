5b1f0e3d155b65125ffd470a7faf8daa172dcf66
Tamuna <tamuna.kakhidze.1@btu.edu.ge>
2021-02-01 15:25:32 +0400

    first commit

---
+++ app/src/main/java/com/example/fragments/Fragments/HomeFragment.kt
@@ -0,0 +1,7 @@
+package com.example.fragments.Fragments
+
+import androidx.fragment.app.Fragment
+import com.example.fragments.R
+
+class HomeFragment : Fragment(R.layout.fragment_home) {
+}
---
+++ app/src/main/java/com/example/fragments/Fragments/PersonFragment.kt
@@ -0,0 +1,7 @@
+package com.example.fragments.Fragments
+
+import androidx.fragment.app.Fragment
+import com.example.fragments.R
+
+class PersonFragment : Fragment(R.layout.fragment_person) {
+}
